c256a6f104fe2cde703efb7667bc5442414d5f39
Ivan Shynkarenka <chronoxor@gmail.com>
2016-07-09 01:23:31 +0300

    Clean code

--- tests/test_template_class.cpp
+++ tests/test_template_class.cpp
@@ -10,12 +10,12 @@ using namespace CppTemplate;
 
 TEST_CASE("Template class", "[CppTemplate]")
 {
-    CppTemplate::Template instance(0);
+    Template instance(0);
 
     REQUIRE(instance.field() == 0);
 
     for (int i = 0; i < 1000; ++i)
         REQUIRE(instance.Method(1000) < 1000);
 
-    REQUIRE(CppTemplate::Template::StaticMethod(1000) == 1000);
+    REQUIRE(Template::StaticMethod(1000) == 1000);
 }
