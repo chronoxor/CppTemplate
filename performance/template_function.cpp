bb90fb39ca6ee442d0b0a712f9350d7c19616c44
Ivan Shynkarenka <chronoxor@gmail.com>
2018-06-12 14:42:27 +0300

    update

--- performance/template_function.cpp
+++ performance/template_function.cpp
@@ -8,17 +8,17 @@
 
 using namespace CppTemplate;
 
-const uint64_t iterations = 10000000;
+const uint64_t operations = 10000000;
 
 BENCHMARK("function")
 {
     uint64_t crc = 0;
 
-    for (uint64_t i = 0; i < iterations; ++i)
+    for (uint64_t i = 0; i < operations; ++i)
         crc += function(1000);
 
     // Update benchmark metrics
-    context.metrics().AddIterations(iterations - 1);
+    context.metrics().AddOperations(operations - 1);
     context.metrics().SetCustom("CRC", crc);
 }
 
