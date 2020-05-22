1028d394b3dc9e8831db93f28384871181d08a30
Travis CI <travis@travis-ci.org>
2020-05-22 22:12:51 +0000

    Update generated documentation

--- search/variables_0.js
+++ search/variables_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['version',['version',['../namespace_cpp_template.html#a456fc2b0df12279e381bdd14f355ddca',1,'CppTemplate']]]
+  ['version_31',['version',['../namespace_cpp_template.html#a456fc2b0df12279e381bdd14f355ddca',1,'CppTemplate']]]
 ];
