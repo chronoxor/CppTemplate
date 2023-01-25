4d33709c8c1e6c47caeca34568f87d66adc3e71c
chronoxor <chronoxor@users.noreply.github.com>
2023-01-25 19:41:07 +0000

    Update generated documentation

--- search/all_0.js
+++ search/all_0.js
@@ -1,8 +1,8 @@
 var searchData=
 [
-  ['class_2ecpp_0',['class.cpp',['../class_8cpp.html',1,'']]],
-  ['class_2eh_1',['class.h',['../class_8h.html',1,'']]],
-  ['class_2einl_2',['class.inl',['../class_8inl.html',1,'']]],
-  ['cpptemplate_3',['CppTemplate',['../namespace_cpp_template.html',1,'']]],
-  ['c_2b_2b_20template_20library_4',['C++ Template Library',['../index.html',1,'']]]
+  ['c_2b_2b_20template_20library_0',['C++ Template Library',['../index.html',1,'']]],
+  ['class_2ecpp_1',['class.cpp',['../class_8cpp.html',1,'']]],
+  ['class_2eh_2',['class.h',['../class_8h.html',1,'']]],
+  ['class_2einl_3',['class.inl',['../class_8inl.html',1,'']]],
+  ['cpptemplate_4',['CppTemplate',['../namespace_cpp_template.html',1,'']]]
 ];
