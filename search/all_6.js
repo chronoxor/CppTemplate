79d6de51c38b702c15d0ecf4b3244a80d6249ecd
chronoxor <chronoxor@users.noreply.github.com>
2026-02-23 01:02:24 +0000

    Update generated documentation

--- search/all_6.js
+++ search/all_6.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['template_0',['template',['../class_cpp_template_1_1_template.html',1,'CppTemplate::Template'],['../class_cpp_template_1_1_template.html#a06387bdda6aadc368b6168ff1a81032f',1,'CppTemplate::Template::Template(int filed) noexcept'],['../class_cpp_template_1_1_template.html#aff5135be217dfdc708a35367c7e25aaa',1,'CppTemplate::Template::Template(const Template &amp;) noexcept=default'],['../class_cpp_template_1_1_template.html#adeb242bed40712a89cc73ed4e8e08749',1,'CppTemplate::Template::Template(Template &amp;&amp;) noexcept=default']]],
+  ['template_0',['Template',['../class_cpp_template_1_1_template.html',1,'CppTemplate::Template'],['../class_cpp_template_1_1_template.html#a06387bdda6aadc368b6168ff1a81032f',1,'CppTemplate::Template::Template(int filed) noexcept'],['../class_cpp_template_1_1_template.html#aff5135be217dfdc708a35367c7e25aaa',1,'CppTemplate::Template::Template(const Template &amp;) noexcept=default'],['../class_cpp_template_1_1_template.html#adeb242bed40712a89cc73ed4e8e08749',1,'CppTemplate::Template::Template(Template &amp;&amp;) noexcept=default']]],
   ['template_20library_1',['C++ Template Library',['../index.html',1,'']]]
 ];
